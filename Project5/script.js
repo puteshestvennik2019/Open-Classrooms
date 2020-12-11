c28886076519c49801bd61ea137a010b707bf6c5
puteshestvennik2019 <puteshestvennik30@gmail.com>
2020-12-11 12:57:28 +0000

    Fixed cart bugs, added tests and finalized POST request

--- Project5/script.js
+++ Project5/script.js
@@ -19,6 +19,9 @@ let selectedItem = {
 	varnish: {}
 };
 
+const MAX_LIMIT_PER_ITEM = 100;
+const MAX_LIMIT_PER_ORDER = 1000;
+
 
 // local storage
 if (isNaN(parseInt(localStorage.getItem('shoppingCart'))))
@@ -155,13 +158,6 @@ function changeVarnish(newColour) {
 	selectedVarnish = newColour;
 }
 
-function updateCartItems() {
-	const cart = document.getElementById('items-in-cart');
-	if (cart != null) {
-		cart.innerHTML = parseInt(localStorage.getItem('shoppingCart'));
-	}
-}
-
 function closePopupWindow () {
 	itemDetailedView.classList.remove('item-detailed-view-active');
 	popupOverlay.classList.remove('overlay-active');
@@ -260,14 +256,6 @@ function addCol(data, title) {
 // Shopping cart
 // =============
 
-let updateQuantityBtns = document.getElementsByClassName('update-quantity');
-
-for (let i = 0; i < updateQuantityBtns.length; i++) {
-	updateQuantityBtns[i].addEventListener('click', () => {
-		updateTotal();
-	});
-}
-
 function updateTotal() {
 	let total = 0;
 	const items = document.getElementById('shopping-cart-items').children;
@@ -275,9 +263,9 @@ function updateTotal() {
 		const qtyElement = items[i].getElementsByClassName('shopping-cart-col-quantity-input')[0];
 
 		// check input before updating
-		checkInput(qtyElement);
+		const qty = checkInput(qtyElement.value);
+		qtyElement.value = qty;
 
-		const qty = qtyElement.value;
 		const price = parseFloat(items[i].getElementsByClassName('shopping-cart-col-price')[0].innerHTML.replace('£',''));
 		let partial = Math.round(price * qty * 100) / 100;
 		total = Math.round((total + partial) * 100) / 100;
@@ -286,33 +274,67 @@ function updateTotal() {
 	totalSpan.innerHTML = '£' + total;
 }
 
-function checkInput(inputElement) {
-	if (isNaN(inputElement.value) || inputElement.value < 0)
-		inputElement.value = 0;
-}
-
-function updateProductInCart() {
-	let itemsInCart = JSON.parse(localStorage.getItem('itemsInCart'));
-	if (itemsInCart == null) {
-		itemsInCart = {
-			[selectedItem.id]: selectedItem
-		}
+function checkInput(input) {
+	if (isNaN(input) || input < 0) {
+		return 0;
 	}
 	else {
-		// if 'brand new' item is being added to shopping cart
-		if (itemsInCart[selectedItem.id] == undefined) {
+		if (input > MAX_LIMIT_PER_ITEM) {
+			alert('To order more than 100 units, please contact our customer service');
+			return MAX_LIMIT_PER_ITEM;
+		}
+		else return input;
+	}
+}
+
+function updateProductInCart(fromCart, newQty = 1) {
+	let itemsInCart = JSON.parse(localStorage.getItem('itemsInCart')); 
+	let prevQty = 0;
+
+	// if updated doesn't come from the cart page, it means the item is already present in local storage object
+	if (!fromCart) {
+		if (itemsInCart == null) {
 			itemsInCart = {
-				...itemsInCart,
 				[selectedItem.id]: selectedItem
 			}
 		}
+		else {
+			// if 'brand new' item is being added to shopping cart
+			if (itemsInCart[selectedItem.id] == undefined) {
+				itemsInCart = {
+					...itemsInCart,
+					[selectedItem.id]: selectedItem
+				}
+			}
+		}
+
+		itemsInCart[selectedItem.id].varnish[selectedVarnish] += newQty;
 	}
-	itemsInCart[selectedItem.id].varnish[selectedVarnish] += 1;
+	// Update was made inside the cart
+	else {
+		prevQty = itemsInCart[selectedItem.id].varnish[selectedVarnish];
+		itemsInCart[selectedItem.id].varnish[selectedVarnish] = newQty;
+
+		// same item with other varnishes might still be in the cart
+		// if not, however, delete the item
+		itemsInCart = checkIfDelete(itemsInCart);
+	}
+
 	localStorage.setItem('itemsInCart', JSON.stringify(itemsInCart));
+
+	// return the difference between old and new qty - default is 1, unless update came from cart.html
+	updateTotalItems(newQty - prevQty);
 }
 
+// this function is called when 'Add To Cart' button is clicked
 function addToCart() {
 	event.preventDefault();
+
+	// prevent ordering more than 1000 items in a single order
+	if (totalItemsInCart > MAX_LIMIT_PER_ORDER) {
+		alert('For wholesale orders, please contact our customer service');
+		return;
+	}
 
 	// if no varnish selected, prompt user
 	if (selectedVarnish == '') {
@@ -320,23 +342,44 @@ function addToCart() {
 		return;
 	}
 
-	// get number of items already added
-	let itemsInCart = parseInt(localStorage.getItem('shoppingCart')) + 1;
+	updateProductInCart(false);
+}
 
-	// update local storage and navbar
-	localStorage.setItem('shoppingCart', itemsInCart);
+function checkIfDelete(itemsInCart) {
+	for (let varn in itemsInCart[selectedItem.id].varnish) {
+		if (itemsInCart[selectedItem.id].varnish[varn] != 0) {
+			return itemsInCart;
+		}
+	}
+
+	// if we got here, there are no varnishes with this ID in the cart
+	itemsInCart[selectedItem.id] = undefined;
+
+	return itemsInCart;
+
+}
+
+// update total number of items in local storage
+function updateTotalItems(change) {
+	let numberOfItems = totalItemsInCart() + change;
+	localStorage.setItem('shoppingCart', numberOfItems);
+
+	// refresh navbar
 	updateCartItems();
-	updateProductInCart();
+
 }
 
-function checkInput(inputElement) {
-	if (isNaN(inputElement.value) || inputElement.value < 0)
-		inputElement.value = 0;
+// update number displayed in navbar
+function updateCartItems() {
+	const cart = document.getElementById('items-in-cart');
+	if (cart != null) {
+		cart.innerHTML = totalItemsInCart();
+	}
 }
 
-// =========
-// cart.html
-// =========
+function totalItemsInCart() {
+	return parseInt(localStorage.getItem('shoppingCart'));
+}
 
 function populateCart() {
 	let cart = JSON.parse(localStorage.getItem('itemsInCart'));
@@ -350,6 +393,11 @@ function populateCart() {
 					const cartRow = document.createElement('div');
 					cartRow.className += "shopping-cart-row";
 					cartRow.style = "height: 120px";
+
+					// DATASET
+					// add dataset for future cart updates
+					cartRow.dataset['id'] = cart[obj].id;
+					cartRow.dataset['varnish'] = varn;
 
 					const item = document.createElement('div');
 					item.className += "shopping-cart-col shopping-cart-col-item";
@@ -392,39 +440,113 @@ function populateCart() {
 			}
 			
 		}
+		updateTotal();
 	}
-
-	updateTotal();
 }
 
 populateCart();
 
-function submitOrder() {
-	checkInput();
-	event.preventDefault();
-	const order = {
-		contact : {
-			firstName : 'fname',
-			lastName : 'lname',
-			address : 'address',
-			city : 'city',
-			email : 'email@email.com'
-		},
-		products : ["5beaadda1c9d440000a57d98", "5beaadda1c9d440000a57d98"]
-	}
+let updateQuantityBtns = document.getElementsByClassName('update-quantity');
 
-	sendToAPI(order);
-
-	// reset form manually
-	// document.getElementById("order-confirmation").reset();
+for (let i = 0; i < updateQuantityBtns.length; i++) {
+	updateQuantityBtns[i].addEventListener('click', () => {
+		updateLocalStorage(event.target);
+	});
 }
 
-function checkInput() {
-	const form = document.getElementById("order-confirmation").children;
-	// for (let i = 0; i < form.length; i++) {
-	// 	console.log(form[i]);
-	// }
+// when 'Update' button is clicked in cart.html
+function updateLocalStorage(updateButton) {
+	let cart = JSON.parse(localStorage.getItem('itemsInCart'));
 
+	// update parent's parent
+	const row = updateButton.parentElement.parentElement;
+
+	// THIS HAS GOTTEN OUT OF HAND...
+	// USING THE GLOBAL VARIABLE IN THE BEGINNING HAS CREATED PROBLEMS NOW
+	// TOO MUCH REFACTORING WOULD BE NECESSARY TO FIX THIS...
+	// THEREFORE, I HAVE TO DO IT THE UGLY WAY...
+	selectedVarnish = row.dataset.varnish;
+	selectedItem.id = row.dataset.id;
+
+	// NOT ideal, but updateTotal checks input, so must be passed first
+	updateTotal();
+
+	const qty = updateButton.parentElement.children[0].value;
+	updateProductInCart(true, qty);
+}
+
+function submitOrder() {
+	event.preventDefault();
+
+	if (!checkForm()) {
+		alert('Please double check the contact details');
+		return;
+	}
+
+	const order = getOrderInfo();
+
+	if (order != undefined)
+		sendToAPI(order);
+
+	// reset form manually and clear localStorage
+	document.getElementById("order-confirmation").reset();
+	localStorage.clear();
+}
+
+function getOrderInfo() {
+	const form = document.getElementById('order-confirmation');
+	const order = {
+		contact : {
+			firstName : form.elements["fname"].value,
+			lastName : form.elements["lname"].value,
+			address : form.elements["address"].value,
+			city : form.elements["city"].value,
+			email : form.elements["email"].value
+		},
+		products : getProdArray()
+	}
+
+	return order;
+}
+
+function getProdArray() {
+	const cart = JSON.parse(localStorage.getItem('itemsInCart'));
+	let itemsLeftToAdd = parseInt(localStorage.getItem('shoppingCart'));
+	const arr = [];
+
+	for (let item in cart) {
+		// iterate over varnishes, as they store quantities
+		for (let varn in cart[item].varnish) {
+			while (cart[item].varnish[varn] > 0) {
+				cart[item].varnish[varn] -= 1;
+				arr.push(item);
+				itemsLeftToAdd -= 1;
+			}
+
+			// break out early, if no items left to add
+			if (itemsLeftToAdd == 0)
+				break;
+		}
+	}
+
+	return arr;
+}
+
+function checkForm() {
+	const form = document.getElementById('order-confirmation');
+
+	// Basic checks are done by HTML
+
+	if (strip(form.elements["fname"].value).length === 0 || strip(form.elements["lname"].value).length === 0 || strip(form.elements["city"].value).length === 0) {
+		return false;
+	}
+
+	return true;
+}
+
+// strips white spaces only, but could also remove digits and punctuation
+function strip(str) {
+	return str.replace(/^\s+|\s+$/g, ''); 
 }
 
 function makeRequest(data) {
@@ -452,15 +574,15 @@ function makeRequest(data) {
 }
 
 async function sendToAPI(data) {
-	// alert('Function called');
 
 	try {
 		const requestPromise = makeRequest(data);
 		const response = await requestPromise;
 
-		alert(response.contact.firstName);
-		alert(response.products[0].name);
-		alert(response.orderId);
+		if (response.orderId != undefined)
+			alert(`Your order ${response.orderId} is being processed`);
+		else
+			alert("Something went wrong");
 	}
 	catch(errorResponse) {
 		alert('Request failed with error: ' + errorResponse.error);
